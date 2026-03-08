43721128af56235481317a126b63ee1f67882a70
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-03-08 07:21:57 -0400

    Dynamic card added, still have some work to do on dynamic card

--- script.js
+++ script.js
@@ -27,9 +27,18 @@ async function getData() {
     const issueCard = document.getElementById("issueCard");
     const issueCards = issues.map(issue => {
         return `
-        <div class="border border-gray-300 rounded-md p-4 mb-4">
+        <div class="mb-3 shadow rounded-md p-4 bg-white border-t-4 ${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}">
+            <div class="TopIcon flex justify-between items-center mb-3">
+                ${issue.status === "open" ? '<img src="./assets/Open-Status.png" alt="">' : '<img src="./assets/Closed- Status .png" alt="">'}
+                ${issue.priority === "high" ? '<p class="bg-red-200 rounded-full py-1 px-7 text-red-500 font-semibold">HIGH</p>' : issue.priority === "medium" ? '<p class="bg-yellow-200 rounded-full py-1 px-7 text-yellow-500 font-semibold">MEDIUM</p>' : '<p class="bg-[#EEEFF2] rounded-full py-1 px-7 text-[#9CA3AF] font-semibold">LOW</p>'}
+            </div>
             <h3 class="text-lg font-semibold mb-2">${issue.title}</h3>
             <p class="text-gray-600 mb-2">${issue.description}</p>
+            <div class="batch flex items-center gap-1 mb-4 uppercase">
+                <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[0]}</div>
+                <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[1]}</div>
+                
+            </div>
             <p class="text-sm text-gray-500">Status: ${issue.status}</p>
         </div>
         `;
