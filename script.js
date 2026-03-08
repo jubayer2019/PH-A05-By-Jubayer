c355df83d949dac251eb8e30e723299e5178cac8
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-03-08 03:43:38 -0400

    Login Page Validation Alart added

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ document.getElementById("signinBtn").addEventListener("click", (event) => {
         window.location.assign("./home.html")
     }
     else if(userNameValue == "" && passwordValue == ""){
-        alert("Enter Username and Password")
+        // alert("Enter Username and Password")
+        userName.placeholder = "*Username is required";
+        password.placeholder = "*Password is required";
+        userName.style.borderColor = "red";
+        password.style.borderColor = "red";
     }
     else{
-        alert("Invalid Username or Password")
+        const Alert = `
+        <div role="alert" class="alert alert-error">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>Invalid Username or Password</span>
+        </div>`
+        document.getElementById("alertContainer").innerHTML = Alert;
     }
 })
 // Login Page End
