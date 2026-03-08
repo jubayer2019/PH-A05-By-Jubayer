e51858f272f5cf9d5770f9a4bc62fb0011dde8b1
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-03-08 11:07:00 -0400

    Dynamic card section updated

--- script.js
+++ script.js
@@ -27,19 +27,38 @@ async function getData() {
     const issueCard = document.getElementById("issueCard");
     const issueCards = issues.map(issue => {
         return `
-        <div class="mb-3 shadow rounded-md p-4 bg-white border-t-4 ${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}">
-            <div class="TopIcon flex justify-between items-center mb-3">
-                ${issue.status === "open" ? '<img src="./assets/Open-Status.png" alt="">' : '<img src="./assets/Closed- Status .png" alt="">'}
-                ${issue.priority === "high" ? '<p class="bg-red-200 rounded-full py-1 px-7 text-red-500 font-semibold">HIGH</p>' : issue.priority === "medium" ? '<p class="bg-yellow-200 rounded-full py-1 px-7 text-yellow-500 font-semibold">MEDIUM</p>' : '<p class="bg-[#EEEFF2] rounded-full py-1 px-7 text-[#9CA3AF] font-semibold">LOW</p>'}
-            </div>
-            <h3 class="text-lg font-semibold mb-2">${issue.title}</h3>
-            <p class="text-gray-600 mb-2">${issue.description}</p>
-            <div class="batch flex items-center gap-1 mb-4 uppercase">
-                <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[0]}</div>
-                <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[1]}</div>
-                
-            </div>
-            <p class="text-sm text-gray-500">Status: ${issue.status}</p>
+        <div>
+        
+        <div class=" shadow rounded-t-md p-4 bg-white border-t-4 ${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}">
+                    <div class="TopIcon flex justify-between items-center mb-3">
+                        ${issue.status === "open" ? '<img src="./assets/Open-Status.png" alt="">' : '<img src="./assets/Closed- Status .png" alt="">'}
+                        ${issue.priority === "high" ? '<p class="bg-red-200 rounded-full py-1 px-7 text-red-500 font-semibold">HIGH</p>' : issue.priority === "medium" ? '<p class="bg-yellow-200 rounded-full py-1 px-7 text-[#D97706] font-semibold">MEDIUM</p>' : '<p class="bg-[#EEEFF2] rounded-full py-1 px-7 text-[#9CA3AF] font-semibold">LOW</p>'}
+                    </div>
+                    <h3 class="text-lg font-semibold mb-2">${issue.title.length > 25 
+                    ? issue.title.slice(0,25) + "..." 
+                    : issue.title}</h3>
+                    <p class="text-gray-600 mb-2">${issue.description.length > 50 
+                    ? issue.description.slice(0,50) + "..." 
+                    : issue.description}</p>
+                    <div class="batch flex items-center gap-1 mb-2 uppercase">
+                        <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[0]}</div>
+                        <div class="badge badge-outline rounded-full bg-[#FFF8DB] badge-warning text-[10px] font-bold">${issue.labels[1]}</div>
+                        
+                    </div>
+                    
+                </div>
+                <div class="shadow mb-4 flex justify-between items-center rounded-b-md p-4 bg-white border-t border-gray-200">
+                    <div>
+                        <p class="text-sm text-gray-500">#${issue.id} by ${issue.author}</p>
+                        <p class="text-sm text-gray-500">Assignee: ${issue.assignee}</p>
+                    </div>
+                    <div class="text-right">
+                        <p class="text-sm text-gray-500">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                        <p class="text-sm text-gray-500">Updated: ${new Date(issue.updatedAt).toLocaleDateString()}</p>
+                    </div>
+                    
+                </div>
+
         </div>
         `;
     }).join("");
